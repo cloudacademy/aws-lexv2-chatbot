1e0f6f6f21e71b99a950b0164fc23494b29abcaf
Jeremy Cook <jeremycook123@gmail.com>
2021-10-19 14:00:41 +1300

    updated

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ AWS_ACCOUNT_ID=$(aws iam get-user | grep Arn | cut -d':' -f6)
 
 find . -name '.DS_Store' -type f -delete
 
+echo
+echo step1: packaging the lambda function...
+echo
 pushd lambda
 rm -rf ./package
 pip3 install --target ./package tmdbv3api
@@ -12,10 +15,16 @@ zip -r ../lambda-movieapi.zip .
 popd
 zip -g lambda-movieapi.zip lambda_function.py
 
+echo
+echo step2: creating lex service linked role...
+echo
 if ! aws iam get-role --role-name AWSServiceRoleForLexBots > /dev/null 2>&1; then
     aws iam create-service-linked-role --aws-service-name lex.amazonaws.com
 fi
 
+echo
+echo step3: creating lambda exec policy...
+echo
 if ! aws iam get-policy --policy-arn arn:aws:iam::$AWS_ACCOUNT_ID:policy/LexChatbotLambdaExecPolicy > /dev/null 2>&1; then
     aws iam create-policy --policy-name LexChatbotLambdaExecPolicy --policy-document file://lambda_exec_iam_policy.json 
 fi
@@ -23,6 +32,9 @@ fi
 LAMBDA_POLICY_ARN=$(aws iam list-policies --query "Policies[?PolicyName == 'LexChatbotLambdaExecPolicy'].Arn" --output text)
 echo LAMBDA_POLICY_ARN=$LAMBDA_POLICY_ARN
 
+echo
+echo step4: creating lambda exec role...
+echo
 if ! aws iam get-role --role-name LexChatbotLambdaExecRole > /dev/null 2>&1; then
     aws iam create-role --role-name LexChatbotLambdaExecRole --assume-role-policy-document file://lambda_trust_policy.json
     aws iam attach-role-policy --policy-arn $LAMBDA_POLICY_ARN --role-name LexChatbotLambdaExecRole
@@ -31,6 +43,9 @@ fi
 LAMBDA_ROLE_ARN=$(aws iam list-roles --query "Roles[?RoleName == 'LexChatbotLambdaExecRole'].Arn" --output text)
 echo LAMBDA_ROLE_ARN=$LAMBDA_ROLE_ARN
 
+echo
+echo step5: creating lambda function...
+echo
 if aws lambda get-function --function-name movierecommendations > /dev/null 2>&1; then
     aws lambda delete-function --function-name movierecommendations
 fi
@@ -43,6 +58,9 @@ aws lambda create-function \
     --environment Variables="{APIKEY=$APIKEY}" \
     --role $LAMBDA_ROLE_ARN
 
+echo
+echo step6: packaging the lex chatbot deployment...
+echo
 popd
 pushd lex
 zip -r ../lex-movierecommendations.zip .
